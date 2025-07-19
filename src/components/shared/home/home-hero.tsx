dcb2d990415ad2f87d3de77d53801357193d6069
Batyr <awbatyr@gmail.com>
2025-07-19 14:58:05 +0500

    build

--- src/components/shared/home/home-hero.tsx
+++ src/components/shared/home/home-hero.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
-import { useMediaQuery } from "usehooks-ts";
-import { useTranslation } from "react-i18next";
+// import { useMediaQuery } from "usehooks-ts";
+// import { useTranslation } from "react-i18next";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import { Link } from "react-router-dom";
 
 export const HomeHero: FC = () => {
-  const { t } = useTranslation("home");
+  // const { t } = useTranslation("home");
   const [emblaRef] = useEmblaCarousel({ loop: true }, [
     Autoplay({
       delay: 5000,
@@ -14,27 +13,25 @@ export const HomeHero: FC = () => {
     }),
   ]);
 
-  const lg = useMediaQuery("(min-width: 1024px)");
-  const md = useMediaQuery("(min-width: 768px)");
+  // const lg = useMediaQuery("(min-width: 1024px)");
+  // const md = useMediaQuery("(min-width: 768px)");
 
-  function getBanners() {
-    if (lg)
-      return t("banners.lg", { returnObjects: true }) as {
-        path: string;
-        link: string;
-      }[];
-    if (md)
-      return t("banners.md", { returnObjects: true }) as {
-        path: string;
-        link: string;
-      }[];
-    return t("banners.sm", { returnObjects: true }) as {
-      path: string;
-      link: string;
-    }[];
-  }
-
-  const banners = getBanners();
+  // function getBanners() {
+  //   if (lg)
+  //     return t("banners.lg", { returnObjects: true }) as {
+  //       path: string;
+  //       link: string;
+  //     }[];
+  //   if (md)
+  //     return t("banners.md", { returnObjects: true }) as {
+  //       path: string;
+  //       link: string;
+  //     }[];
+  //   return t("banners.sm", { returnObjects: true }) as {
+  //     path: string;
+  //     link: string;
+  //   }[];
+  // }
 
   return (
     <div className="mb-6">
